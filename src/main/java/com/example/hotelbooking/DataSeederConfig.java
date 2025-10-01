304088d33c17c174c611c5076c70bb908763c759
2300032353 <2300032353@kluniversity.in>
2025-10-01 10:57:52 +0530

    Remove BOM characters from Java files

--- src/main/java/com/example/hotelbooking/DataSeederConfig.java
+++ src/main/java/com/example/hotelbooking/DataSeederConfig.java
@@ -8,13 +8,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataSeederConfig {
+
     @Bean
-    CommandLineRunner seedHotels(HotelRepository repository) {
+    public CommandLineRunner seedHotels(HotelRepository repository) {
         return args -> {
             if (repository.count() == 0) {
                 Hotel h1 = new Hotel(null, "Sunrise Resort", "Goa", "Beach Road", 10, 1200.0);
                 Hotel h2 = new Hotel(null, "Hilltop Inn", "Ooty", "Valley View", 5, 1800.0);
                 Hotel h3 = new Hotel(null, "City Comfort", "Hyderabad", "Main Street", 20, 900.0);
+
                 repository.save(h1);
                 repository.save(h2);
                 repository.save(h3);
