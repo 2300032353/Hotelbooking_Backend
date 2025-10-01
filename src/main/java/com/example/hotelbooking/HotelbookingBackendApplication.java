ca0188e07e66a1947ec11e1004e5c0dc0c37794e
2300032353 <2300032353@kluniversity.in>
2025-10-01 14:33:03 +0530

    completed

--- src/main/java/com/example/hotelbooking/HotelbookingBackendApplication.java
+++ src/main/java/com/example/hotelbooking/HotelbookingBackendApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelbookingBackendApplication {
+public class HotelBookingBackendApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(HotelbookingBackendApplication.class, args);
+        SpringApplication.run(HotelBookingBackendApplication.class, args);
     }
 }
