66580e348675723778e95310dac9d3596bcc8388
2300032353 <2300032353@kluniversity.in>
2025-10-01 20:55:28 +0530

    Third commit

--- src/main/java/com/example/hotelbooking/HotelBookingBackendApplication.java
+++ src/main/java/com/example/hotelbooking/HotelBookingBackendApplication.java
@@ -1,19 +1,13 @@
-package com.example.hotelbooking;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 @SpringBootApplication
 public class HotelBookingBackendApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HotelBookingBackendApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HotelBookingBackendApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HotelBookingBackendApplication.class);
+    }
 }
+
